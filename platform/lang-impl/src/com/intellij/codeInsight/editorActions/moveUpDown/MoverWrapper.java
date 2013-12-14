f8354de76a575a4cff960a3543c84d4e73ac5678
Vojtech Krasa <vojta.krasa@gmail.com>
2013-12-14 13:57:34 +0100

    IDEA-80056 replaced assert back to condition, as multi cursors need that in cases when one line can be moved and other not.

--- platform/lang-impl/src/com/intellij/codeInsight/editorActions/moveUpDown/MoverWrapper.java
+++ platform/lang-impl/src/com/intellij/codeInsight/editorActions/moveUpDown/MoverWrapper.java
@@ -45,7 +45,9 @@ class MoverWrapper {
   }
 
   public final void move(Editor editor, final PsiFile file) {
-    assert myInfo.toMove2 != null;
+    if (myInfo.toMove2 == null) {
+      return;
+    }
     myMover.beforeMove(editor, myInfo, myIsDown);
     final Document document = editor.getDocument();
     final int start = StatementUpDownMover.getLineStartSafeOffset(document, myInfo.toMove.startLine);
